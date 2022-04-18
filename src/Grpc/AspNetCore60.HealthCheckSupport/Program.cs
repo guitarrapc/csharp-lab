2f1775ad2d9d0a36dc481db44cb72e13341d04bf
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-04-18 13:11:27 +0900

    feat: add Grpc Server

---
+++ src/Grpc/AspNetCore60.HealthCheckSupport/Program.cs
@@ -0,0 +1,21 @@
+using AspNetCore60.HealthCheckSupport.Services;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddGrpcHealthChecks()
+                .AddCheck("Sample", () => HealthCheckResult.Healthy());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGrpcHealthChecksService();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ src/Grpc/README.md
@@ -0,0 +1,6 @@
+gRPC in 1 minutes (C#)
+========================
+
+ASP.NET Core Grpc with Health Probe.
+
+> see: https://docs.microsoft.com/en-us/aspnet/core/grpc/health-checks?view=aspnetcore-6.0
