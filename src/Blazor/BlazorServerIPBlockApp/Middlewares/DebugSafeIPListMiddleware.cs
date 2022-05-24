29ff4b0fee8b959087b3179ccbc059b83cb3118c
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-05-24 18:13:09 +0900

    feat: IPNetwork detection

--- src/Blazor/BlazorServerIPBlockApp/Middlewares/DebugSafeIPListMiddleware.cs
+++ src/Blazor/BlazorServerIPBlockApp/Middlewares/DebugSafeIPListMiddleware.cs
@@ -6,7 +6,7 @@ public class DebugSafeIPListMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger<DebugSafeIPListMiddleware> _logger;
-    private readonly byte[][] _safelist;
+    private readonly IPNetwork[] _ipNetworks;
 
     public DebugSafeIPListMiddleware(RequestDelegate next, ILogger<DebugSafeIPListMiddleware> logger, DebugSafeIPOption option)
     {
@@ -16,19 +16,24 @@ public class DebugSafeIPListMiddleware
         // ; separated IP list string. "8.8.8.8;1.1.1.1;4.4.4.4"
         if (!option.Enabled)
         {
-            _safelist = Array.Empty<byte[]>();
+            _ipNetworks = Array.Empty<IPNetwork>();
         }
         else
         {
-            ArgumentNullException.ThrowIfNull(option.IPList);
-
-            var ips = option.IPList;
-            _safelist = new byte[ips.Length][];
-            for (var i = 0; i < ips.Length; i++)
+            if (option.IpNetworks is null)
             {
-                if (IPAddress.TryParse(ips[i], out var ip))
+                _ipNetworks = Array.Empty<IPNetwork>();
+            }
+            else
+            {
+                var ipNetworks = option.IpNetworks;
+                _ipNetworks = new IPNetwork[ipNetworks.Length];
+                for (var i = 0; i < _ipNetworks.Length; i++)
                 {
-                    _safelist[i] = ip.GetAddressBytes();
+                    if (IPNetwork.TryParse(ipNetworks[i], out var ip))
+                    {
+                        _ipNetworks[i] = ip;
+                    }
                 }
             }
         }
@@ -37,43 +42,29 @@ public class DebugSafeIPListMiddleware
     public async Task Invoke(HttpContext context)
     {
         var remoteIp = context.Connection.RemoteIpAddress;
-
         _logger.LogInformation("Request from Remote IP address: {RemoteIP}", remoteIp);
-        if (remoteIp is null)
+
+        if (remoteIp is not null && _ipNetworks.Length > 0)
         {
-            await _next(context);
-        }
-        else if (_safelist.Length == 0)
-        {
-            await _next(context);
-        }
-        else
-        {
-            var bytes = remoteIp.GetAddressBytes();
-            var badIp = true;
-            foreach (var address in _safelist)
+            if (remoteIp.IsIPv4MappedToIPv6)
             {
-                if (address.SequenceEqual(bytes))
-                {
-                    badIp = false;
-                    break;
-                }
+                remoteIp = remoteIp.MapToIPv4();
             }
 
-            if (badIp)
+            if (!_ipNetworks.Any(x => x.Contains(remoteIp)))
             {
                 _logger.LogWarning("Forbidden request from remote IP Address: {RemoteIP}", remoteIp);
                 context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
                 return;
             }
-
-            await _next.Invoke(context);
         }
+
+        await _next.Invoke(context);
     }
 }
 
 public class DebugSafeIPOption
 {
     public bool Enabled { get; set; }
-    public string[]? IPList { get; set; }
+    public string[]? IpNetworks { get; set; }
 }
