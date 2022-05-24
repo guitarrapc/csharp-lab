2551f006a8c0477a313c1f661113ecea22df91bb
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-05-24 19:03:10 +0900

    feat: add server name

--- src/Blazor/BlazorServerIPBlockApp/Pages/Index.razor.cs
+++ src/Blazor/BlazorServerIPBlockApp/Pages/Index.razor.cs
@@ -7,12 +7,14 @@ public partial class Index
     [Inject]
     private IHttpContextAccessor Accessor { get; set;  } = default!;
 
+    private string _serverName = "";
     private IDictionary<string, string> _headers = default!;
-    private string _remoteIps = "";
+    private string _remoteIp = "";
 
     protected override void OnInitialized()
     {
+        _serverName = Environment.MachineName;
         _headers = Accessor.HttpContext!.Request.Headers.ToDictionary(kv => kv.Key.ToString(), kv => kv.Value.ToString());
-        _remoteIps = Accessor.HttpContext!.Connection.RemoteIpAddress!.ToString();
+        _remoteIp = Accessor.HttpContext!.Connection.RemoteIpAddress!.ToString();
     }
 }
