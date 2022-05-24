32b010d53bafc96b83a8a90c12171e5e114a2981
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-05-24 17:16:51 +0900

    feat: show header

--- src/Blazor/BlazorServerIPBlockApp/Pages/Index.razor.cs
+++ src/Blazor/BlazorServerIPBlockApp/Pages/Index.razor.cs
@@ -7,11 +7,13 @@ public partial class Index
     [Inject]
     private IHttpContextAccessor Accessor { get; set;  } = default!;
 
+    private IDictionary<string, string> _headers = default!;
     private string _xForwardedHost = "";
     private string _remoteIps = "";
 
     protected override void OnInitialized()
     {
+        _headers = Accessor.HttpContext!.Request.Headers.ToDictionary(kv => kv.Key.ToString(), kv => kv.Value.ToString());
         if (Accessor.HttpContext!.Request.Headers.TryGetValue("X-Forwarded-Host", out var host))
         {
             _xForwardedHost = host;
