53251fc26a83a8b705b712931a2df8d61243dae3
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-06-28 18:09:32 +0900

    feat: add Stream Logging

--- src/Logger/LoggerBlazorApp/Program.cs
+++ src/Logger/LoggerBlazorApp/Program.cs
@@ -11,18 +11,18 @@ builder.Services.AddLogging(configure =>
     configure.ClearProviders();
 
     var logLevelStr = Environment.GetEnvironmentVariable("CSHARPLAB_LOG_LEVEL", EnvironmentVariableTarget.Process) ?? "Information";
-    var enableConsoleLoggingStr = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_CONSOLE_LOGGING", EnvironmentVariableTarget.Process) ?? "true";
-    var enableFileLoggingStr = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_FILE_LOGGING", EnvironmentVariableTarget.Process) ?? "false";
-    var enableStreamLoggingStr = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_STREAM_LOGGING", EnvironmentVariableTarget.Process) ?? "false";
+    var enableConsoleLogging = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_CONSOLE_LOGGING", EnvironmentVariableTarget.Process) ?? "true";
+    var enableFileLogging = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_FILE_LOGGING", EnvironmentVariableTarget.Process) ?? "false";
+    var enableStreamLogging = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_STREAM_LOGGING", EnvironmentVariableTarget.Process) ?? "false";
     var fileLoggingPath = Environment.GetEnvironmentVariable("CSHARPLAB_FILE_LOGGING_PATH", EnvironmentVariableTarget.Process) ?? $"./app.log";
     var streamLoggingHost = Environment.GetEnvironmentVariable("CSHARPLAB_STREAM_LOGGING_HOST", EnvironmentVariableTarget.Process) ?? "127.0.0.1";
     var streamLoggingPortStr = Environment.GetEnvironmentVariable("CSHARPLAB_STREAM_LOGGING_PORT", EnvironmentVariableTarget.Process) ?? "12345";
 
     Console.WriteLine(@$"[Logging] Debugging Configuration:
   CSHARPLAB_LOG_LEVEL: {logLevelStr}
-  CSHARPLAB_ENABLE_CONSOLE_LOGGING: {enableConsoleLoggingStr}
-  CSHARPLAB_ENABLE_FILE_LOGGING: {enableFileLoggingStr}
-  CSHARPLAB_ENABLE_STREAM_LOGGING: {enableStreamLoggingStr}
+  CSHARPLAB_ENABLE_CONSOLE_LOGGING: {enableConsoleLogging}
+  CSHARPLAB_ENABLE_FILE_LOGGING: {enableFileLogging}
+  CSHARPLAB_ENABLE_STREAM_LOGGING: {enableStreamLogging}
   CSHARPLAB_FILE_LOGGING_PATH: {fileLoggingPath}
   CSHARPLAB_STREAM_LOGGING_HOST: {streamLoggingHost}
   CSHARPLAB_STREAM_LOGGING_PORT: {streamLoggingPortStr}");
@@ -34,13 +34,13 @@ builder.Services.AddLogging(configure =>
     }
 
     // Console Logging
-    if (enableConsoleLoggingStr == "true")
+    if (enableConsoleLogging == "true")
     {
         Console.WriteLine("[Logging] Console Logging Enabled.");
         configure.AddZLoggerConsole();
     }
     // File Logging
-    if (enableFileLoggingStr == "true")
+    if (enableFileLogging == "true")
     {
         if (!string.IsNullOrEmpty(fileLoggingPath))
         {
@@ -49,12 +49,12 @@ builder.Services.AddLogging(configure =>
         }
     }
     // Stream Logging
-    if (enableStreamLoggingStr == "true")
+    if (enableStreamLogging == "true")
     {
-        if (!int.TryParse(streamLoggingPortStr, out var port))
+        if (int.TryParse(streamLoggingPortStr, out var port))
         {
             var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(streamLoggingHost, port);
+            socket.RetryableSocketConnect(streamLoggingHost, port);
             var network = new NetworkStream(socket);
 
             Console.WriteLine("[Logging] Stream Logging Enabled.");
@@ -85,3 +85,24 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
+public static class LoggingExtentions
+{
+    public static void RetryableSocketConnect(this Socket socket, string host, int port, int retryCount = 5, int retryMaxSecond = 10)
+    {
+        var current = 1;
+        do
+        {
+            try
+            {
+                socket.Connect(host, port);
+            }
+            catch (SocketException) when (current < retryCount)
+            {
+                var sleep = (int)Math.Min(1 * Math.Pow(2, current -1), retryMaxSecond);
+                Console.WriteLine($"Retry connecting socket. Host: {host}; Port: {port}; Current: {current++}; RetryLimit: {retryCount}; Sleep: {sleep}");
+                Thread.Sleep(1000 * sleep);
+            }
+        } while (current <= retryCount);
+    }
+}
