176e75d6ff8b4148b1744537b91930a3b3a30ac6
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-06-28 19:21:38 +0900

    refactor: capsule to method

--- src/Logger/LoggerBlazorApp/Program.cs
+++ src/Logger/LoggerBlazorApp/Program.cs
@@ -6,61 +6,7 @@ using ZLogger;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddLogging(configure =>
-{
-    configure.ClearProviders();
-
-    var logLevelStr = Environment.GetEnvironmentVariable("CSHARPLAB_LOG_LEVEL", EnvironmentVariableTarget.Process) ?? "Information";
-    var enableConsoleLogging = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_CONSOLE_LOGGING", EnvironmentVariableTarget.Process) ?? "true";
-    var enableFileLogging = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_FILE_LOGGING", EnvironmentVariableTarget.Process) ?? "false";
-    var enableStreamLogging = Environment.GetEnvironmentVariable("CSHARPLAB_ENABLE_STREAM_LOGGING", EnvironmentVariableTarget.Process) ?? "false";
-    var fileLoggingPath = Environment.GetEnvironmentVariable("CSHARPLAB_FILE_LOGGING_PATH", EnvironmentVariableTarget.Process) ?? $"./app.log";
-    var streamLoggingHost = Environment.GetEnvironmentVariable("CSHARPLAB_STREAM_LOGGING_HOST", EnvironmentVariableTarget.Process) ?? "127.0.0.1";
-    var streamLoggingPortStr = Environment.GetEnvironmentVariable("CSHARPLAB_STREAM_LOGGING_PORT", EnvironmentVariableTarget.Process) ?? "12345";
-
-    Console.WriteLine(@$"[Logging] Debugging Configuration:
-  CSHARPLAB_LOG_LEVEL: {logLevelStr}
-  CSHARPLAB_ENABLE_CONSOLE_LOGGING: {enableConsoleLogging}
-  CSHARPLAB_ENABLE_FILE_LOGGING: {enableFileLogging}
-  CSHARPLAB_ENABLE_STREAM_LOGGING: {enableStreamLogging}
-  CSHARPLAB_FILE_LOGGING_PATH: {fileLoggingPath}
-  CSHARPLAB_STREAM_LOGGING_HOST: {streamLoggingHost}
-  CSHARPLAB_STREAM_LOGGING_PORT: {streamLoggingPortStr}");
-
-    // Fatal, Error, Warning, Information, Debug, Trace
-    if (Enum.TryParse<LogLevel>(logLevelStr, true, out var logLevel))
-    {
-        configure.SetMinimumLevel(logLevel);
-    }
-
-    // Console Logging
-    if (enableConsoleLogging == "true")
-    {
-        Console.WriteLine("[Logging] Console Logging Enabled.");
-        configure.AddZLoggerConsole();
-    }
-    // File Logging
-    if (enableFileLogging == "true")
-    {
-        if (!string.IsNullOrEmpty(fileLoggingPath))
-        {
-            Console.WriteLine($"[Logging] File Logging Enabled.");
-            configure.AddZLoggerFile(fileLoggingPath);
-        }
-    }
-    // Stream Logging
-    if (enableStreamLogging == "true")
-    {
-        if (int.TryParse(streamLoggingPortStr, out var port))
-        {
-            Console.WriteLine("[Logging] Stream Logging Enabled.");
-            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            socket.RetryableSocketConnect(streamLoggingHost, port);
-            var network = new NetworkStream(socket);
-            configure.AddZLoggerStream(network);
-        }
-    }
-});
+builder.ConfigureLogging();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
@@ -87,7 +33,63 @@ app.Run();
 
 public static class LoggingExtentions
 {
-    public static void RetryableSocketConnect(this Socket socket, string host, int port, int retryCount = 5, int retryMaxSecond = 10)
+    public static void ConfigureLogging(this WebApplicationBuilder builder)
+    {
+        var logLevelStr = builder.Configuration.GetValue<string?>("CSHARPLAB_LOG_LEVEL") ?? "Information";
+        var enableConsoleLogging = builder.Configuration.GetValue<bool?>("CSHARPLAB_ENABLE_CONSOLE_LOGGING") ?? true;
+        var enableFileLogging = builder.Configuration.GetValue<bool?>("CSHARPLAB_ENABLE_FILE_LOGGING") ?? false;
+        var enableStreamLogging = builder.Configuration.GetValue<bool?>("CSHARPLAB_ENABLE_STREAM_LOGGING") ?? false;
+        var fileLoggingPath = builder.Configuration.GetValue<string?>("CSHARPLAB_FILE_LOGGING_PATH") ?? "./app.log";
+        var streamLoggingHost = builder.Configuration.GetValue<string?>("CSHARPLAB_STREAM_LOGGING_HOST") ?? "127.0.0.1";
+        var streamLoggingPort = builder.Configuration.GetValue<int?>("CSHARPLAB_STREAM_LOGGING_PORT") ?? 12345;
+
+        builder.Services.AddLogging(configure =>
+        {
+            configure.ClearProviders();
+
+            Console.WriteLine(@$"[Logging] Debugging Configuration:
+  CSHARPLAB_LOG_LEVEL: {logLevelStr}
+  CSHARPLAB_ENABLE_CONSOLE_LOGGING: {enableConsoleLogging}
+  CSHARPLAB_ENABLE_FILE_LOGGING: {enableFileLogging}
+  CSHARPLAB_ENABLE_STREAM_LOGGING: {enableStreamLogging}
+  CSHARPLAB_FILE_LOGGING_PATH: {fileLoggingPath}
+  CSHARPLAB_STREAM_LOGGING_HOST: {streamLoggingHost}
+  CSHARPLAB_STREAM_LOGGING_PORT: {streamLoggingPort}");
+
+            // Fatal, Error, Warning, Information, Debug, Trace
+            if (Enum.TryParse<LogLevel>(logLevelStr, true, out var logLevel))
+            {
+                configure.SetMinimumLevel(logLevel);
+            }
+
+            // Console Logging
+            if (enableConsoleLogging)
+            {
+                Console.WriteLine("[Logging] Console Logging Enabled.");
+                configure.AddZLoggerConsole();
+            }
+            // File Logging
+            if (enableFileLogging)
+            {
+                if (!string.IsNullOrEmpty(fileLoggingPath))
+                {
+                    Console.WriteLine($"[Logging] File Logging Enabled.");
+                    configure.AddZLoggerFile(fileLoggingPath);
+                }
+            }
+            // Stream Logging
+            if (enableStreamLogging)
+            {
+                Console.WriteLine("[Logging] Stream Logging Enabled.");
+                var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.RetryableSocketConnect(streamLoggingHost, streamLoggingPort);
+                var network = new NetworkStream(socket);
+                configure.AddZLoggerStream(network);
+            }
+        });
+    }
+
+    private static void RetryableSocketConnect(this Socket socket, string host, int port, int retryCount = 5, int retryMaxSecond = 10)
     {
         var current = 1;
         do
