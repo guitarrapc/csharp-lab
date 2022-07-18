2ce720ab750e8ce3497cf1753606ea76d414efff
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-07-18 23:38:01 +0900

    feat: attribute

--- src/SourceGenerator/SourceGeneratorBasic.ConsoleApp/NotifyClass.cs
+++ src/SourceGenerator/SourceGeneratorBasic.ConsoleApp/NotifyClass.cs
@@ -2,12 +2,12 @@ namespace SourceGeneratorBasic.ConsoleApp;
 
 public partial class NotifyClass
 {
-    [MyAutoNotifyAttribute]
+    [MyAutoNotify.MyAutoNotifyAttribute]
     private bool _boolProp;
 
-    [MyAutoNotifyAttribute]
+    [MyAutoNotify.MyAutoNotifyAttribute]
     private int _intProp;
 
-    [MyAutoNotifyAttribute(PropertyName = "Nanika")]
+    [MyAutoNotify.MyAutoNotifyAttribute(PropertyName = "Nanika")]
     private long _longProp;
 }
