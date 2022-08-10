9eed2e0edeb0ccab992fe9586a0b734f38e9058e
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-08-10 11:37:41 +0900

    feat: add Database project for EF with Sqlite (file)

---
+++ src/Database/DatabaseSqliteEF/BloggingContextFactory.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using DatabaseCore.Models;
+using System.Reflection;
+
+namespace DatabaseMySqlEf;
+
+// Context for Designtime. see https://docs.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli
+public class BloggingContextFactory : IDesignTimeDbContextFactory<BloggingDbContext>
+{
+    public BloggingDbContext CreateDbContext(string[] args)
+    {
+        var dbPath = Path.Combine(Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.FullName, "Blogging.db");
+        var optionsBuilder = new DbContextOptionsBuilder<BloggingDbContext>();
+        optionsBuilder.UseSqlite($"Data Source={dbPath}", options =>
+        {
+            options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+        });
+
+        return new BloggingDbContext(optionsBuilder.Options);
+    }
+}
---
+++ src/Database/DatabaseSqliteEF/README.md
@@ -0,0 +1,14 @@
+Initialize with dotnet-ef
+```bash
+cd src/Database/DatabaseSqliteEF
+
+# only for once you created project
+dotnet new tool-manifest
+dotnet tool install dotnet-ef
+
+# Create migration
+dotnet ef migrations add InitialCreate
+
+# Run migration
+dotnet ef database update
+```
