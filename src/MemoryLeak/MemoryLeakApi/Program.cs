3626aa76874c3f3351e457b1ab4801ee60b522da
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-06-07 17:18:41 +0900

    feat: add MemoryLeak codes

---
+++ src/MemoryLeak/MemoryLeakApi/Program.cs
@@ -0,0 +1,29 @@
+using MemoryLeak.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+var allocator = new MemoryAllocator();
+
+// Configure the HTTP request pipeline.
+app.MapGet("/", (HttpRequest request) => allocator.AllocateStaticString(5120)); // 10KB
+app.MapGet("/alloc_static", (HttpRequest request) => allocator.AllocateStaticString(GetSize(request.Query, 5120))); // 10KB
+app.MapGet("/alloc_str", (HttpRequest request) => allocator.AllocateString(GetSize(request.Query, 5120))); // 10KB
+app.MapGet("/alloc_loh", (HttpRequest request) => allocator.AllocateLoH(GetSize(request.Query, 10240))); // 10KB
+app.MapGet("/alloc_pool", (HttpRequest request) => allocator.AllocateArrayPoolArray(GetSize(request.Query, 8192))); // 10KB
+app.MapGet("/clear", () => allocator.Clear());
+app.MapGet("/gc", () => {
+    GC.Collect();
+    GC.Collect();
+});
+
+app.Run();
+
+static int GetSize(IQueryCollection query, int defaultSize)
+{
+    var sizeStr = query["size"];
+    var size = int.TryParse(sizeStr, out var value) ? value : 0;
+    return size;
+}
