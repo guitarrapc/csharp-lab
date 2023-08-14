eed8e65c8f4c59a52e5e0bb99f664cfb36d4317e
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-08-15 01:22:33 +0900

    feat: add cidrBlock benchmark

---
+++ src/Logic/LogicLab.Benchmark/Networks/CidrBlockBenchmarks.cs
@@ -0,0 +1,46 @@
+using BenchmarkDotNet.Attributes;
+using LogicLab.Networks;
+
+namespace LogicLab.Benchmark.Networks;
+
+[ShortRunJob]
+[MemoryDiagnoser]
+[MinColumn, MaxColumn]
+public class CidrBlockBenchmarks
+{
+    [Benchmark]
+    public void CtorString()
+    {
+        for (byte i = 0; i < 10; i++)
+        {
+            new CidrBlock("10.0.0.1/24");
+        }
+    }
+
+    [Benchmark]
+    public void CtorBytes()
+    {
+        for (byte i = 0; i < 10; i++)
+        {
+            new CidrBlock(10, 0, 0, 1, 24);
+        }
+    }
+
+    [Benchmark]
+    public void TryParseString()
+    {
+        for (byte i = 0; i < 10; i++)
+        {
+            CidrBlock.TryParse("10.0.0.1/24", out var cidr);
+        }
+    }
+
+    [Benchmark]
+    public void TryParseBytes()
+    {
+        for (byte i = 0; i < 10; i++)
+        {
+            CidrBlock.TryParse(10, 0, 0, 1, 24, out var cidr);
+        }
+    }
+}
