56319df9c0c4091554c06c5c6c44024a47ae3df1
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-11-17 14:56:13 +0900

    feat: add CpuModelTest

--- src/SystemInfo/SystemInfo.Tests/CpuModelTest.cs
+++ src/SystemInfo/SystemInfo.Tests/CpuModelTest.cs
@@ -4,12 +4,13 @@ namespace SystemInfo.Tests;
 
 public class CpuModelTest
 {
+    private static CpuModel cpuModel = CpuModel.Current;
+
     [Fact]
     public void Windows_x86_64_Test()
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && RuntimeInformation.ProcessArchitecture == Architecture.X64)
         {
-            var cpuModel = CpuModel.Current;
             Assert.NotEqual("Unkown", cpuModel.ModelName);
             Assert.Empty(cpuModel.UnkownReason);
         }
@@ -20,7 +21,6 @@ public class CpuModelTest
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
         {
-            var cpuModel = CpuModel.Current;
             Assert.NotEqual("Unkown", cpuModel.ModelName);
             Assert.Empty(cpuModel.UnkownReason);
         }
@@ -31,7 +31,6 @@ public class CpuModelTest
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && RuntimeInformation.ProcessArchitecture == Architecture.X64)
         {
-            var cpuModel = CpuModel.Current;
             Assert.NotEqual("Unkown", cpuModel.ModelName);
             Assert.Empty(cpuModel.UnkownReason);
         }
@@ -42,7 +41,6 @@ public class CpuModelTest
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
         {
-            var cpuModel = CpuModel.Current;
             Assert.NotEqual("Unkown", cpuModel.ModelName);
             Assert.Empty(cpuModel.UnkownReason);
         }
@@ -53,7 +51,6 @@ public class CpuModelTest
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && RuntimeInformation.ProcessArchitecture == Architecture.X64)
         {
-            var cpuModel = CpuModel.Current;
             Assert.NotEqual("Unkown", cpuModel.ModelName);
             Assert.Empty(cpuModel.UnkownReason);
         }
@@ -64,7 +61,6 @@ public class CpuModelTest
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
         {
-            var cpuModel = CpuModel.Current;
             Assert.NotEqual("Unkown", cpuModel.ModelName);
             Assert.Empty(cpuModel.UnkownReason);
         }
