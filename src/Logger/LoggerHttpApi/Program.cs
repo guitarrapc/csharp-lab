8998bb3d35d8070bc7ad1b79c52a22756d275602
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-11-17 19:42:47 +0900

    chore: replace to OpenApi

--- src/Logger/LoggerHttpApi/Program.cs
+++ src/Logger/LoggerHttpApi/Program.cs
@@ -1,9 +1,8 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 // ==== HttpLogging ====
 {
@@ -67,8 +66,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || enableSwagger)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 // ==== HttpLogging ====
